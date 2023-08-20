041ae42c27d1228a3b2bd335d273aae8b65724d7
Younes <younes.slimani@yahoo.fr>
2023-08-20 15:31:30 +0100

    Initialisation

---
+++ principale.php
@@ -0,0 +1,43 @@
+ 
+<html>
+ <head>
+ <meta charset="utf-8">
+ <!-- importer le fichier de style -->
+ <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+ </head>
+ <body style='background:#fff;'>
+ <div id="content">
+ 
+ <a href='principale.php?deconnexion=true'><span>Déconnexion</span></a>
+ 
+ <!-- tester si l'utilisateur est connecté -->
+ <?php
+ session_start();
+ if(isset($_GET['deconnexion']))
+ { 
+ if($_GET['deconnexion']==true)
+ { 
+ session_unset();
+ header("location:login.php");
+ }
+ }
+ /* else if($_SESSION['username'] !== ""){
+ $user = $_SESSION['username'];
+ // afficher un message
+ echo "<br>Bonjour $user, vous êtes connectés";
+ } */
+
+
+ if(isset($_SESSION['username'])) 
+ { 
+     $user = $_SESSION['username'];
+     echo "<br>Bonjour--- $user ----, vous êtes 1111111111111";}
+ else { 
+    header("location:login.php");
+}
+
+ ?>
+ 
+ </div>
+ </body>
+ </html>
